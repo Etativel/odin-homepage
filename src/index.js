6cf5f9f1990e92ecf489557ef4dbbe077f3af161
Etativel <farhanmaulana4611@gmail.com>
2024-10-30 15:27:44 +0800

    feat: add responsive navbar and header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("Hello!");
+import "./styles/style.css";
+// import profilePict from "./assets/photos/profile-picture.jpg";
+import profilePict from "./assets/photos/pexels-isaak-alexandre-karslian-2027804005-29097920.jpg";
+const header = document.querySelector("header");
+header.style.backgroundImage = `url(${profilePict})`;
