7fe757da9677fbae09b73688f7d4e85d43d3d0ae
Etativel <farhanmaulana4611@gmail.com>
2024-10-31 17:24:03 +0800

    feat: upadte image resolution

--- src/js/imageModule/assetsLoader.js
+++ src/js/imageModule/assetsLoader.js
@@ -1,7 +1,7 @@
 import profilePict from "../../assets/photos/pexels-isaak-alexandre-karslian-2027804005-29097920.jpg";
 import IBMCarbonReduced from "../../assets/dummy-projects/IBMCarbon2.png";
-import IBMCarbonOrigin from "../../assets/dummy-projects/IBMCarbon.png";
-import PixLab from "../../assets/dummy-projects/pixLab.png";
+import IBMCarbonOrigin from "../../assets/dummy-projects/IBMCarbon3.png";
+import PixLab from "../../assets/dummy-projects/pixLab3.png";
 import PixLabReducee from "../../assets/dummy-projects/pixLabOpac.png";
 
 function loadImage() {
